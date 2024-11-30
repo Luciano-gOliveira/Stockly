e1ccdcfcb7530cf8764cc4ff301561d6e0750e08
Luciano Oliveira <aulascursos66@gmail.com>
2024-11-30 18:38:26 -0400

    chore: add initial tailwind and prettier setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from 'next/font/google'
 import "./globals.css";
+import Sidebar from "./_components/sidebar";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'auto',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +21,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} antialiased`}
       >
-        {children}
+        <div className="h-full flex">
+          <Sidebar />
+          {children}
+        </div>
       </body>
     </html>
   );
