c875ad22c3de70816f9ead2181983df45da6f39b
Luciano Oliveira <aulascursos66@gmail.com>
2024-12-09 21:24:29 -0400

    refactor: add route handlers

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -5,23 +5,29 @@ import { ProductTableColumns } from "./_components/product-columns.table";
 import { getProducts } from "../_data-access/product/get-products";
 
 const ProductsPage = async () => {
-  const products = await getProducts()
+  const products = await getProducts();
 
   return (
     <div className="w-full px-8">
-      <div className="flex justify-between items-center py-4 pt-6">
+      <div className="flex items-center justify-between py-4 pt-6">
         <div>
-            <span className="text-gray-500">produtos</span>
-            <h2 className="font-bold text-xl">Gestão de produtos</h2>
-        </div>  
+          <span className="text-gray-500">produtos</span>
+          <h2 className="text-xl font-bold">Gestão de produtos</h2>
+        </div>
         <Button>
-            <PlusIcon/>
-            Novo Produto
+          <PlusIcon />
+          Novo Produto
         </Button>
-      </div>  
-      <div className="bg-white rounded-sm">
-        <DataTable data={products} columns={ProductTableColumns} />
       </div>
+      <div className="rounded-sm bg-white">
+        <DataTable
+          // data={JSON.parse(JSON.stringify(products))}
+          // columns={ProductTableColumns}
+          data={JSON.parse(JSON.stringify(products))}
+          columns={ProductTableColumns}
+        />
+      </div>
+ 
     </div>
   );
 };
