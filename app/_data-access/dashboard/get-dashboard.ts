0d113b17239b7ad041e7b7590cb9f8d27e11734f
Luciano Oliveira <aulascursos66@gmail.com>
2025-02-12 18:11:14 -0300

    feat: add chart and data acces function of 14lastdays

--- app/_data-access/dashboard/get-dashboard.ts
+++ app/_data-access/dashboard/get-dashboard.ts
@@ -1,5 +1,11 @@
 import "server-only";
 import { db } from "@/app/_lib/prisma";
+import dayjs from "dayjs"
+
+interface DayTotalRevenue {
+  day: Date
+  totalRevenue: number
+}
 
 interface DashboardDto {
   totalRevenue: number;
@@ -7,10 +13,35 @@ interface DashboardDto {
   totalSales: number;
   totalStock: number;
   totalProducts: number;
+  totalLast14DaysRevenue: DayTotalRevenue[];
 }
 
 //TODO: ajeitar essa bagaça pra não ter que fazer query SQL
 export const GetDashboard = async (): Promise<DashboardDto> => {
+  const today = dayjs().endOf("day").toDate()
+  const last14days = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map( day => {
+    return dayjs(today).subtract(day, "day")
+  })
+
+  const totalLast14DaysRevenue: DayTotalRevenue[] = [] 
+  for (const day of last14days){
+    const dayTotalRevenue = await db.$queryRawUnsafe<{totalRevenue: number}[]>(
+      `
+        SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
+        FROM "SaleProduct"
+        WHERE "createdAt" > $1 AND "createdAt" < $2;
+      `,
+      day.startOf("day").toDate(),
+      day.endOf("day").toDate()
+    )
+    // console.log({totalLast14DaysRevenue})
+    totalLast14DaysRevenue.push({
+      day: day.toDate(),
+      totalRevenue: dayTotalRevenue[0].totalRevenue
+    })
+  }
+
+
   const totalRevenueQuery = `
     SELECT SUM("unitPrice" * "quantity") as "totalRevenue"
     FROM "SaleProduct"
@@ -53,5 +84,6 @@ export const GetDashboard = async (): Promise<DashboardDto> => {
     totalSales,
     totalStock: Number(totalStock._sum.stock),
     totalProducts,
+    totalLast14DaysRevenue,
   };
 };
