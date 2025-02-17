66e0719d488eda97f7f4f0cdd50a1ac888a2f656
Luciano Oliveira <aulascursos66@gmail.com>
2025-02-17 19:38:30 -0300

    refactor: delete unused imports

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,31 +1,5 @@
 "use client";
-import { Badge } from "@/app/_components/ui/badge";
-import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import {
-  CircleIcon,
-  ClipboardCopy,
-  Edit2Icon,
-  MoreHorizontalIcon,
-  Trash2Icon,
-} from "lucide-react";
-import DeleteDialogContent from "./delete-dialog-content";
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-} from "@/app/_components/ui/alert-dialog";
-import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/app/_components/ui/dropdown-menu";
-import { Button } from "@/app/_components/ui/button";
-import UpsertProductContent from "./upsert-product-content";
-import { useState } from "react";
 import ProductTableDropdownMenu from "./table-dropdown-menu";
 import { ProductDto } from "@/app/_data-access/product/get-products";
 import ProductStatusBadge from "@/app/_components/product-status-badge";
